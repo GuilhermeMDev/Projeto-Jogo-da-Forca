6134e854f096e0cdbc45141df0f16cb862e252d7
GuilhermeMDev <guimagioni@gmail.com>
2022-04-22 21:34:22 -0300

    Refatorando e atualizando o projeto

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 #Desenvolvendo um jogo da forca do zero! Projeto de conclusão do curso ruby  basico..
-#Não quis copiar nem olhar nenhum conteudo antes de tentar fazer algo pra chegar prox da ideia de um jogo da forca.
+#Do andamento... Buscando uma forma de fixar a palavra sorteada, e desenvolver como o usuario vai testar varias letras até completar ou acertar a palavra..
+#Vou inserir dicas ao sorte palavra
+#E também uma ASCII interagindo com os erros e acertos durante o jogo..
 
 require_relative 'lib/sortear_palavra'
 
@@ -7,13 +9,15 @@ require_relative 'lib/sortear_palavra'
 #Instanciando o objeto
 jogo = SortearPalavra.sortear
 @check = jogo.split(//)
+
+#iniciando o jogo.
 puts "Bem vindo ao jogo da forca! \nChute uma letra: "
 @chute = gets.chomp
 puts "A palavra sorteada é #{jogo.upcase}"
 
 #Aqui eu consegui chegar num ponto, onde cada execução me gera uma palavra e permite o usuario inserir uma string, caracter, pra comparar com o array.
 def comparador
-    @check.include?(@chute) do #comparando o valor inserido com o aray 
+    @check.include?(@chute) do #comparando o valor inserido com o array 
     end    
 end
 
